c7e1a8a678ab940829da2efc37abac0dc2714e2f
kesharwaninikhil <kesharwani.nikhil@gmail.com>
2019-06-25 11:05:06 +0530

    SpringBoot + Apache CXF SOAP WS

---
+++ apache-cxf-ws/src/main/java/com/example/service/GreetingService.java
@@ -0,0 +1,20 @@
+package com.example.service;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+
+import com.example.model.Greeting;
+
+@WebService(serviceName = "GreetingService")
+public interface GreetingService {
+
+	@WebMethod()
+	@WebResult(name = "Greeting")
+	public Greeting sayHello(@WebParam(name = "GreetingsRequest") String name);
+
+	@WebMethod()
+	@WebResult(name = "Greeting")
+	public Greeting sayBye(@WebParam(name = "GreetingsRequest") String name);
+}
