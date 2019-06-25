a487fd1eb81dd6040033d8cd156c6ec2042ecd4e
kesharwaninikhil <kesharwani.nikhil@gmail.com>
2019-06-25 10:28:35 +0530

    SpringBoot + SOAP WS

---
+++ soapwebservices/src/main/java/com/example/soapwebservices/SoapwebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.soapwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapwebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapwebservicesApplication.class, args);
+	}
+
+}
